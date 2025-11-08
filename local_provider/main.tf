5c585226d5a94837e62a25e18d889629e39837c1
Rishi Srinivasan Kanaka Sabapathy <rishi.srinivasan95@gmail.com>
2025-11-08 16:41:51 +0100

    feat: add local_sensitive_file resource and create TF_NOTES_SENSITIVE.txt

--- local_provider/main.tf
+++ local_provider/main.tf
@@ -4,3 +4,10 @@ resource "local_file" "tf_notes" {
 This file was created using the local_file resource in Terraform.
 EOT
 }
+
+resource "local_sensitive_file" "tf_notrs" {
+  filename = "${path.module}/TF_NOTES_SENSITIVE.txt"
+  content = <<EOT
+This sensitive file was created using the local_sensitive_file resource in Terraform.
+EOT
+}
